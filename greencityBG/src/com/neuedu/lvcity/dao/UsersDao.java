5668c305fcf1c321a2fae0292f25d227b1da243f
'linkaihu' <'674066248@qq.com'>
2018-06-24 13:43:24 +0800

    学校项目保存

---
+++ greencityBG/src/com/neuedu/lvcity/dao/UsersDao.java
@@ -0,0 +1,5 @@
+package com.neuedu.lvcity.dao;
+import com.neuedu.lvcity.model.*;
+public interface UsersDao {
+public Users login(String username,String password);
+}
---
+++ greencityBG/src/com/neuedu/lvcity/service/impl/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author admin
+ *
+ */
+package com.neuedu.lvcity.service.impl;
